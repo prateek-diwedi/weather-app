6418dd03eaa5f0b25b2ea7699b00740be02ccf50
Prateek Diwedi <diwedi1112@gmail.com>
2020-04-14 13:19:35 +0530

    updated basic structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+
+import "weather-icons/css/weather-icons.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Weather from "../src/components/weather.component"
+
+let api = process.env.REACT_APP_API_KEY;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Weather/>
     </div>
   );
 }
