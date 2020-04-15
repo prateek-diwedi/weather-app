fc8eecfbeb80ba00be1f69706b28615fd7fcce24
Prateek Diwedi <diwedi1112@gmail.com>
2020-04-15 13:11:39 +0530

    weather app fully functional now

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css"
 import "weather-icons/css/weather-icons.css"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Weather from "../src/components/weather.component"
+import Form from "../src/components/form.component"
 
 let api_key = process.env.REACT_APP_API_KEY;
 
@@ -21,29 +22,88 @@ class App extends React.Component {
       description: "",
       error: false
     };
-    this.getWeather();
+
+
+    this.weatherIcon = {
+      Thunderstorm: "wi-thunderstorm",
+      Drizzle: "wi-sleet",
+      Rain: "wi-storm-showers",
+      Snow: "wi-snow",
+      Atmosphere: "wi-fog",
+      Clear: "wi-day-sunny",
+      Clouds: "wi-day-fog"
+    }
   }
 
-  getWeather = async () => {
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${api_key}`)
+  calCelsius(temp) {
+    let cell = Math.floor(temp - 273.15)
+    return cell
+  }
 
-    const response = await api_call.json();
-    console.log('response --->>', response)
+  // changing icons depending on weather code
+  get_weatherIcon(icons, rangeId) {
+    switch (true) {
+      case rangeId >= 200 && rangeId <= 232:
+        this.setState({ icon: this.weatherIcon.Thunderstorm })
+        break;
+      case rangeId >= 300 && rangeId <= 332:
+        this.setState({ icon: this.weatherIcon.Drizzle })
+        break;
+      case rangeId >= 500 && rangeId <= 532:
+        this.setState({ icon: this.weatherIcon.Rain })
+        break;
+      case rangeId >= 600 && rangeId <= 632:
+        this.setState({ icon: this.weatherIcon.Snow })
+        break;
+      case rangeId >= 700 && rangeId <= 781:
+        this.setState({ icon: this.weatherIcon.Atmosphere })
+        break;
+      case rangeId === 800:
+        this.setState({ icon: this.weatherIcon.Clear })
+        break;
+      case rangeId >= 801 && rangeId <= 832:
+        this.setState({ icon: this.weatherIcon.Clouds })
+        break;
+      default:
+        this.setState({ icon: this.weatherIcon.Clouds })
+    }
+  }
 
-    this.setState({
-      city: response.name,
-      country: response.sys.country,
-      celsius: response.main.temp,
-      temp_max: response.main.temp_max,
-      temp_min: response.main.temp_min,
-      description: response.weather[0].description,
-      main: response.weather[0].main
-    })
+  getWeather = async (e) => {
+    e.preventDefault();
+
+    //getting city name from form
+    let city = e.target.elements.city.value
+
+    //getting country name from form
+    let country = e.target.elements.country.value
+
+    if (city && country) {
+      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${api_key}`)
+
+      const response = await api_call.json();
+      console.log('response --->>', response)
+
+            
+      this.setState({
+        city: `${response.name}, ${response.sys.country}`,
+        celsius: this.calCelsius(response.main.temp),
+        temp_max: this.calCelsius(response.main.temp_max),
+        temp_min: this.calCelsius(response.main.temp_min),
+        description: response.weather[0].description,
+        main: response.weather[0].main,
+      })
+
+      this.get_weatherIcon(this.weatherIcon, response.weather[0].id)
+    } else {
+      this.setState({ error: true })
+    }
   }
 
   render() {
     return (
       <div className="App">
+        <Form loadweather={this.getWeather} error={this.state.error} />
         <Weather
           city={this.state.city}
           country={this.state.country}
@@ -52,6 +112,7 @@ class App extends React.Component {
           temp_min={this.state.temp_min}
           description={this.state.description}
           main={this.state.main}
+          weatherIcon={this.state.icon}
         />
       </div>
     )
--- src/components/weather.component.jsx
+++ src/components/weather.component.jsx
@@ -3,13 +3,16 @@ import React from "react";
 const Weather = (props) => {
 
   return (
-    <div className="container">
-      <div className="cards">
-        <h1>{props.city}, {props.country}</h1>
+    <div className="container text-light">
+      <div className="cards pt-4">
+        <h1>{props.city}</h1>
         <h5 className="py-4">
-          <i className="wi wi-day-sunny display-1"></i>
+          <i className={`wi ${props.weatherIcon} display-1`}></i>
         </h5>
-        <h1 className="py-2">{props.temp_celsius}&deg;</h1>
+        
+        {props.temp_celsius ? (<h1 className="py-2">{props.temp_celsius}&deg;</h1>) : null}
+
+        {/* show min max temp */}
         {minmaxTemp(props.temp_max, props.temp_min)}
         <h2 className="py-4">{props.main}</h2>
         <h4 className="py-3">{props.description}</h4>
@@ -19,12 +22,14 @@ const Weather = (props) => {
 };
 
 function minmaxTemp(min, max) {
+  if(min && max) {
   return (
     <h3>
       <span className="px-4">{min}&deg;</span>
       <span className="px-4">{max}&deg;</span>
     </h3>
   )
+  }
 }
 
 export default Weather;
